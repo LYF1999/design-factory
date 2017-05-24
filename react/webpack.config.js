708258879378cdb758eb8c57366a710fffff1ef2
LYF1999 <lyfmagic99@gmail.com>
2017-05-24 08:54:23 +0800

    fix webpack

--- react/webpack.config.js
+++ react/webpack.config.js
@@ -1,7 +1,6 @@
 /* eslint-disable */
 
 module.exports = function (webpackConfig, env) {
-  console.log(webpackConfig.module);
   webpackConfig.module.loaders.push({
     test: /\.(jpe?g|png|gif)$/i,
     loader: 'file?name=[name].[ext]',
