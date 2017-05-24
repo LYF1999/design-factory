7ea6a403e11812dcf447bf4ca7df5bd2896515a6
LYF1999 <lyfmagic99@gmail.com>
2017-05-24 09:02:44 +0800

    fix webpack

--- react/webpack.config.js
+++ react/webpack.config.js
@@ -2,8 +2,11 @@
 
 module.exports = function (webpackConfig, env) {
   webpackConfig.module.loaders.push({
-    test: /\.(jpe?g|png|gif)$/i,
-    loader: 'file?name=[name].[ext]',
+    test: /\.(jpe?g|png|gif|ttf)$/i,
+    loader: 'file',
+    query: {
+      name: 'static/[name].[ext]'
+    }
   });
   return webpackConfig;
 };
